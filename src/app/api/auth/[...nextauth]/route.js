4914a5df0d0d4f5f186402f76265d40ebc42f123
rkerem <recep.kerem1d@gmail.com>
2024-12-25 07:24:06 +0300

    Updating phoneNumber settings -19

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -16,9 +16,7 @@ const handler = NextAuth({
   ],
   session: { strategy: 'jwt' },
   jwt: {
-    // You can define a custom secret or rely on NEXTAUTH_SECRET
     secret: process.env.NEXTAUTH_SECRET,
-    // maxAge: 60 * 60 * 48,
     
   },
   
@@ -26,12 +24,7 @@ const handler = NextAuth({
     
      
     async jwt({ token, account, profile, user }) {
-      // jwt() is called whenever a token is created/updated.
-      // if (account?.access_token) {
-      //   token.accessToken = account.access_token;
-      // }
-      // return token;
-      // If user just signed in, store user details in the token
+      
       if (user) {
         token.email = user.email;
         token.name = user.name;
@@ -40,19 +33,12 @@ const handler = NextAuth({
       return token;
     },
     async session({ session, token }) {
-      // session() callback is called whenever a session is checked(e.g. getSession())
-      // if (token?.accessToken) {
-      //   session.accessToken = token.accessToken;
-      // }
-      // return session;
-      // Expose the *entire* NextAuth token in the session,
-      // so you can pass it to Flask.
+      
       const signedJWT = jwt.sign(token, process.env.NEXTAUTH_SECRET, {
         algorithm: "HS256",
       });
       session.myCustomToken = signedJWT;
 
-      //user info
       session.email = token.email;
       session.name = token.name;
 
