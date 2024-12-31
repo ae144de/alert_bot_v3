e44dfaca0a5b100765a249ec802b6fd627380b34
rkerem <recep.kerem1d@gmail.com>
2024-12-31 19:29:18 +0300

    Fix AlertTable to display id instead of alert_id in TableCell

--- src/app/AlertTable.jsx
+++ src/app/AlertTable.jsx
@@ -137,7 +137,7 @@ export default function AlertTable() {
           <TableBody>
             {alerts.map((alert) => (
               <TableRow key={alert.id}>
-                <TableCell>{alert.alert_id}</TableCell>
+                <TableCell>{alert.id}</TableCell>
                 <TableCell>{alert.type}</TableCell>
                 <TableCell>
                   {alert.status === "Active" ? (
