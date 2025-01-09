791fb46f7d088c93144b6f6c48fda6a2b6064fae
rkerem <recep.kerem1d@gmail.com>
2025-01-09 11:37:23 +0300

    Import dayjs for date manipulation in AlertForm component

--- src/app/AlertForm.jsx
+++ src/app/AlertForm.jsx
@@ -9,7 +9,7 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-
+import dayjs from 'dayjs';
 
 
 export default function AlertForm({ alertType, onClose, onSubmit, onBack }) {
